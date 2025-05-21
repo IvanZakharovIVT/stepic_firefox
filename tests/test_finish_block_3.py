18e017c75d04c83866eb7f6b0568c1e793b407db
ivan <ivan.zakharov@nordclan.com>
2025-05-21 14:54:47 +0400

    Правки именования переменной

--- tests/test_finish_block_3.py
+++ tests/test_finish_block_3.py
@@ -2,7 +2,7 @@ import time
 
 from selenium.webdriver.common.by import By
 
-def test_finish_block_3(browser):
+def test_check_add_to_card_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
     button_item = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
