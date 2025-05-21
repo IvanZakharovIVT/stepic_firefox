0f622db114f53c2b152357ee7c28156daab84612
ivan <ivan.zakharov@nordclan.com>
2025-05-21 14:06:35 +0400

    Слип

--- tests/test_finish_block_3.py
+++ tests/test_finish_block_3.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium.webdriver.common.by import By
 
 def test_finish_block_3(browser):
@@ -5,3 +7,4 @@ def test_finish_block_3(browser):
     browser.get(link)
     button_item = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
     assert button_item.is_displayed()
+    time.sleep(30)
