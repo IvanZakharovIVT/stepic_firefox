6feeb0cc678f4dc109e7205086d9772c17da2e15
ivan <ivan.zakharov@nordclan.com>
2025-05-21 14:01:35 +0400

    Правки теста

--- tests/test_finish_block_3.py
+++ tests/test_finish_block_3.py
@@ -3,5 +3,5 @@ from selenium.webdriver.common.by import By
 def test_finish_block_3(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    button_list = [element.text for element in browser.find_elements(By.XPATH, "//button[@type='submit']")]
-    assert "Добавить в корзину" in button_list
+    button_item = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    assert button_item.is_displayed()
